058d600ff83318fd0415e4116e4257edd68d9288
Avi Caspe <avi.caspe@gmail.com>
2018-01-08 11:02:46 -0800

    Figured out how Dive works.

--- src/Tile.hs
+++ src/Tile.hs
@@ -8,23 +8,24 @@ module Tiles where
 -- score is the score after a merge
 -- newTile generates a new tile randomly
 class Tile a where
+  newTile :: Int -> Maybe a
   canMerge :: a -> a -> Bool
   merge :: a -> a -> a
   score :: a -> a -> Int
-  -- TODO Find the correct type signature for randomness
-  -- newTile :: generator -> (generator, a)
 
 -- 0 is the empty tile
 newtype T2048 = T2048 Int deriving (Show)
 newtype TDive = TDive Int deriving (Show)
 
 instance Tile T2048 where
+  newTile x = if x == 0 then Nothing else Just $ T2048 x
   canMerge (T2048 x) (T2048 y) = x == y
   merge (T2048 x) (T2048 y) = T2048 $ x + y
-  score (T2048 x) (T2048 y) = min x y
+  score (T2048 x) (T2048 y) = x + y
 
 -- TODO Figure out how Dive actually works
 instance Tile TDive where
-  canMerge (TDive x) (TDive y) = _
+  newTile x = if x == 0 then Nothing else Just $ T2048 x
+  canMerge (TDive x) (TDive y) = x `mod` y == 0 || y `mod`x == 0
   merge (TDive x) (TDive y) = TDive $ x + y
-  score (TDive x) (TDive y) = _
+  score (TDive x) (TDive y) = min x y
